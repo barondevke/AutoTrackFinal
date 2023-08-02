b2270e2af6919f8cd023794acd37cad4dd14949a
Steve Baron <ndabasteve1@gmail.com>
2023-08-02 18:52:36 +0300

    Added the props

--- src/Components/CarPage.jsx
+++ src/Components/CarPage.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
-export default function CarPage() {
+
+export default function CarPage({ cars }) {
+    const { index } = useParams()
+    const car = cars[index]
 
     return (
         <div>
-            <img className="carImage" src="https://images7.alphacoders.com/528/528732.jpg"></img>
+            <img className="carImage" src={car.url}></img>
             <Row className="carInfo">
                 <Col  >
-                    <h3 className="carName">Car name</h3>
-                    <h4 className="carLocation">Location: Nairobi, Central</h4>
+                    <h3 className="carName">{car.year} {car.model} {car.make} </h3>
+                    <h4 className="carLocation">{car.location}</h4>
                 </Col>
                 <Col className="carPrice"  >
-                    <h1>Ksh 200000</h1>
-                    <h4 className="carRating">Rating</h4>
+                    <h1>{car.price}</h1>
+                    <h4 className="carRating">{car.rating}</h4>
                 </Col>
             </Row>
             <Row>
                 <Col className="stats">
                     <Row>
                         <Col>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
+                            <h4>Kms Driven</h4>
+                            <h4>Transmission</h4>
+                            <h4>Fuel</h4>
+                            <h4>Consumption</h4>
+
                         </Col>
                         <Col className="rightStats">
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
-                            <h6>Stats</h6>
+                            <h4>{car.kmsDriven}</h4>
+                            <h4>{car.transmissionType}</h4>
+                            <h4>{car.fuelType}</h4>
+                            <h4>{car.fuelConsumption}</h4>
+
                         </Col>
 
                     </Row>
