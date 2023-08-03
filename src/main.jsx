d1b64fbcb2c8675e02e8da24db6217e38c2462a9
CherylYegon <cyegon2023@gmail.com>
2023-08-03 11:31:10 +0300

    Updated css file

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Sellpage from './Sellpage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Sellpage/>
     </BrowserRouter>
   </React.StrictMode>,
 )
