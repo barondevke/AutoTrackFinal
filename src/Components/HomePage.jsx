156d792ae1214f99a823e68536beadf39ecd6662
Steve Baron <ndabasteve1@gmail.com>
2023-08-03 16:45:56 +0300

    added new files

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function HomePage () {
+const HomePage = ({ isLoggedIn, userName }) => {
     return (
-        <h1>Make home page here</h1>
-    )
-}
+        <div className='homepage'>
+            <div className='logo-container'>
+                <img
+                    src="/images/Car Logotype Premium Logo_ ilustración de stock 681412297 _ Shutterstock.jpeg"
+                    alt="Carlogo"
+                    className='logo-img logo1'
+                />
+                <img
+                    src="/images/AT_ Monogram of Two letters A & T.jpeg"
+                    alt="Car 2"
+                    className='logo-img logo2'
+                />
+            </div>
+            {isLoggedIn ? <h2>Welcome back, {userName}!</h2> : null}
+
+            <div className='home1'>
+                <h1>
+                    Your One-Stop Destination for <span style={{ color: 'orange' }}>Buying</span> and{' '}
+                    <span style={{ color: 'orange' }}>Selling</span> Quality Cars
+                </h1>
+
+                <div className='car-image-container'>
+                    <img src="/images/Audi Q3 Sportback SUV.jpeg" alt="" className='car-image' />
+                </div>
+            </div>
+
+            <p className='watermark'>AutoTrack</p>
+
+
+            <p>
+                <Link to="/login">Login to your account</Link>
+            </p>
+
+
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,43 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase.jsx"
+
+const Login = ({ changeForm }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                alert(`WelcomeBack ${email}`)
+            })
+            .catch(() => {
+                alert("User not Found");
+            });
+    };
+
+    return (
+        <div className="sign-in-container">
+            <form onSubmit={signIn}>
+                <h1>Log In to your Account</h1>
+                <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                ></input>
+                <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button type="submit">Log In</button>
+            </form>
+            <button onClick={() => { changeForm("register") }} >Don't have an account</button>
+        </div>
+    );
+};
+
+export default Login;
