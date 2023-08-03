b88b58d196b80797d2f0166f388c3f38aeee6792
Steve Baron <ndabasteve1@gmail.com>
2023-08-03 15:35:56 +0300

    added navbar,carArray,app

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { NavLink , Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-export default function NavBar () {
-  
+export default function NavBar() {
+
     const handleScrollToFooter = () => {
         const footerElement = document.getElementById('footer');
         if (footerElement) {
-          footerElement.scrollIntoView({ behavior: 'smooth' });
+            footerElement.scrollIntoView({ behavior: 'smooth' });
         }
-      };
+    };
 
     return (
         <nav>
             <div className="logo">
-            <i className="fa-solid fa-car" ></i>
-            <span className="logo-text">Auto Track</span>
-         </div>
+                <i className="fa-solid fa-car" ></i>
+                <span className="logo-text">Auto Track</span>
+            </div>
             <ul>
-                <li><NavLink exact to = "/buy">Buy</NavLink></li>
-                <li><NavLink exact to = "/sell">Sell</NavLink></li>
-                <li><Link to= "#"  onClick={handleScrollToFooter}>Contact us</Link></li>
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink exact to="/buy">Buy</NavLink></li>
+                <li><NavLink exact to="/sell">Sell</NavLink></li>
+                <li><NavLink exact to="/about">About</NavLink></li>
+                <li><Link to="#" onClick={handleScrollToFooter}>Contact us</Link></li>
             </ul>
         </nav>
     )
